f13982c43ac7ebeb5e10646fbdc129e2e470184d
fqqzq <1847200440@qq.com>
2016-09-18 21:24:16 +0800

    update 01

--- git_02/src/com/atguigu/git/GitTest.java
+++ git_02/src/com/atguigu/git/GitTest.java
@@ -3,6 +3,13 @@ package com.atguigu.git;
 public class GitTest {
 	public static void main(String[] args) {
 		System.out.println("aaaa");
+		System.out.println("bbbb");
+		System.out.println("aaaa");
+		System.out.println("bbbb");
+		System.out.println("aaaa");
+		System.out.println("bbbb");
+		System.out.println("aaaa");
+		System.out.println("bbbb");
 	}
 
 }
