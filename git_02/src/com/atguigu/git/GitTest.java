b331f8963919238705599ff1b053dd10ac01439f
fqqzq <1847200440@qq.com>
2016-09-18 21:27:41 +0800

    s
    s

--- git_02/src/com/atguigu/git/GitTest.java
+++ git_02/src/com/atguigu/git/GitTest.java
@@ -4,12 +4,10 @@ public class GitTest {
 	public static void main(String[] args) {
 		System.out.println("aaaa");
 		System.out.println("bbbb");
-		System.out.println("aaaa");
-		System.out.println("bbbb");
-		System.out.println("aaaa");
-		System.out.println("bbbb");
-		System.out.println("aaaa");
-		System.out.println("bbbb");
+			System.out.println("bbbb");
+				System.out.println("bbbb");
+					System.out.println("bbbb");
+						System.out.println("bbbb");
 	}
 
 }
